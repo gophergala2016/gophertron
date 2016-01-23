9a8451b3af53ed038a6ef0d887293f803c5d1636
Vibhav Pant <vibhavp@gmail.com>
2016-01-23 12:51:11 +0530

    Add initial models

---
+++ models/field.go
@@ -0,0 +1,84 @@
+package models
+
+import (
+	"sync"
+)
+
+type ChangeDirection struct {
+	Gopher    *Gopher
+	Direction Direction
+	Wait      *sync.WaitGroup
+}
+
+type Field struct {
+	Gophers []*Gopher
+	Board   [100][100]bool
+	Change  chan ChangeDirection
+}
+
+func (f *Field) Add(g *Gopher) error {
+
+	return nil
+}
+
+func (f *Field) increment(g *Gopher) bool {
+	g.Path = append(g.Path, Coordinate{g.X, g.Y})
+	switch g.Direction {
+	case North:
+		g.Y++
+	case South:
+		g.Y--
+	case East:
+		g.X++
+	case West:
+		g.X--
+	}
+
+	if g.X == 100 || g.Y == 100 {
+		return true
+	}
+	if f.Board[g.X][g.Y] {
+		return true
+	}
+
+	f.Board[g.X][g.Y] = true
+	return false
+}
+
+func (f *Field) clearPath(g *Gopher) {
+	for _, c := range g.Path {
+		f.Board[c.X][c.Y] = false
+	}
+}
+
+func (f *Field) Start() {
+	for {
+		select {
+		case dir := <-f.Change:
+			dir.Gopher.Direction = dir.Direction
+			dir.Wait.Done()
+		default:
+			for i, gopher := range f.Gophers {
+				if f.increment(gopher) {
+					//gopher collided, clear it's path and remove it
+					//from the field
+					f.clearPath(gopher)
+					f.Gophers, f.Gophers[len(f.Gophers)-1] = append(f.Gophers[:i], f.Gophers[i+1:]...), nil
+					if len(f.Gophers) == 1 {
+						f.End()
+						return
+					}
+				}
+				f.Broadcast()
+			}
+		}
+	}
+}
+
+func (f *Field) End() {
+
+}
+
+func (f *Field) Broadcast() {
+
+}
---
+++ models/gopher.go
@@ -0,0 +1,25 @@
+package models
+
+type Direction byte
+
+const (
+	North Direction = 1 << iota
+	South
+	East
+	West
+)
+
+type Coordinate struct {
+	X, Y int
+}
+
+type Gopher struct {
+	// Current direction
+	Direction Direction
+	X, Y      int
+	Path      []Coordinate
+}
+
+func (g *Gopher) Increment() {
+
+}
