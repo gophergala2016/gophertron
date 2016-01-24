8d310bd4047a71eccf88f0a2e1fcfa19f8cab4a6
Vibhav Pant <vibhavp@gmail.com>
2016-01-24 12:49:36 +0530

    Add main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"log"
+
+	"github.com/gophergala2016/gophertron/routes"
+)
+
+var addr = flag.String("http", "localhost:8080", "http service address")
+
+func main() {
+	routes.InitRoutes()
+	log.Println("Serving on ", *addr)
+	http.ListenAndServe(*addr, http.DefaultServeMux)
+}
