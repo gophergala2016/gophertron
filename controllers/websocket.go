d034800f9691b7cb508b52708f607e7ee0afb21c
Vibhav Pant <vibhavp@gmail.com>
2016-01-24 12:49:02 +0530

    Add sendPath

--- controllers/websocket.go
+++ controllers/websocket.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"log"
 	"sync"
+	"time"
 
 	"github.com/gophergala2016/gophertron/models"
 	"github.com/gorilla/websocket"
@@ -16,33 +17,55 @@ var dirMap = map[string]models.Direction{
 }
 
 func listener(conn *websocket.Conn, ID int, field *models.Field) {
+	tick := time.NewTicker(5 * time.Millisecond)
 	for {
-		var req struct {
-			Request string
-			Param   string
-		}
+		select {
+		case <-tick.C:
+			var req struct {
+				Request string
+				Param   string
+			}
 
-		err := conn.ReadJSON(&req)
-		if err != nil {
-			log.Println(err)
+			err := conn.ReadJSON(&req)
+			if err != nil {
+				field.Remove <- ID
+				log.Println(err)
+				return
+			}
+
+			switch req.Request {
+			case "move":
+				dir, ok := dirMap[req.Param]
+				if !ok {
+					continue
+				}
+
+				wait := new(sync.WaitGroup)
+				wait.Add(1)
+				field.Change <- models.ChangeDirection{
+					Index:     ID,
+					Direction: dir,
+					Wait:      wait,
+				}
+				wait.Wait()
+			}
+		}
+	}
+}
+
+func sendPath(conn *websocket.Conn, paths chan map[int][]models.Coordinate, close chan struct{}) {
+	mu := new(sync.Mutex)
+
+	for {
+		select {
+		case paths := <-paths:
+			go func() {
+				mu.Lock()
+				conn.WriteJSON(paths)
+				mu.Unlock()
+			}()
+		case <-close:
 			return
-		}
-
-		switch req.Request {
-		case "move":
-			dir, ok := dirMap[req.Param]
-			if !ok {
-				continue
-			}
-
-			wait := new(sync.WaitGroup)
-			wait.Add(1)
-			field.Change <- models.ChangeDirection{
-				Index:     ID,
-				Direction: dir,
-				Wait:      wait,
-			}
-			wait.Wait()
 		}
 	}
 }
