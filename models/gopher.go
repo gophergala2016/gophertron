6019eea9f23cd8a2f02cae134cd95f0ea65963b7
Vibhav Pant <vibhavp@gmail.com>
2016-01-24 12:48:36 +0530

    Add NewGopher(), Paths and Close channels

--- models/gopher.go
+++ models/gopher.go
@@ -19,4 +19,13 @@ type Gopher struct {
 	X, Y      int
 	Path      []Coordinate
 	Score     int
+	Paths     chan map[int][]Coordinate
+	Close     chan struct{}
+}
+
+func NewGopher() *Gopher {
+	return &Gopher{
+		Paths: make(chan map[int][]Coordinate),
+		Close: make(chan struct{}),
+	}
 }
