e6fb6bb97f5576d4bc5c4fb296a958d21cec3f7a
Vibhav Pant <vibhavp@gmail.com>
2016-01-24 15:26:28 +0530

    Add routes for game, websocket and profiler

--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,23 @@ package routes
 
 import (
 	"net/http"
+	_ "net/http/pprof"
+
+	"html/template"
 
 	"github.com/gophergala2016/gophertron/controllers"
+	"github.com/gophergala2016/gophertron/models"
 )
 
 func Main(w http.ResponseWriter, r *http.Request) {
-
+	tmpl := template.Must(template.ParseFiles("./views/active.html"))
+	tmpl.Execute(w, models.GetGames())
 }
 
 func InitRoutes() {
 	http.HandleFunc("/", Main)
 	http.HandleFunc("/create", controllers.Create)
 	http.HandleFunc("/join", controllers.Join)
+	http.HandleFunc("/game", controllers.Game)
+	http.HandleFunc("/websocket", controllers.WebSocket)
 }
