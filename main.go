1ee7a55d2bcb62883eff60fb01310d1e0e8a6af0
Vibhav Pant <vibhavp@gmail.com>
2016-01-24 15:25:07 +0530

    Set log flags

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var addr = flag.String("http", "localhost:8080", "http service address")
 
 func main() {
 	routes.InitRoutes()
+	log.SetFlags(log.Lshortfile)
 	log.Println("Serving on ", *addr)
 	http.ListenAndServe(*addr, http.DefaultServeMux)
 }
